419a1251d6cf4428532e9a582366f8df13dc65f7
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-08-26 12:30:10 -0400

    StoreTest

---
+++ src/test/scala/StoreTest.scala
@@ -0,0 +1,18 @@
+import akka.testkit.TestProbe
+import config.ConfigFactory
+
+class StoreTest extends TestHelperWithKit {
+  import Store._
+  val toDataBase = mockFunction[ExperimentData, Unit]
+  val config = ConfigFactory.makeSquare
+  val killSwitchProbe = TestProbe()
+
+  val store = system.actorOf(Store.props(config, killSwitchProbe.ref, toDataBase))
+
+  "!EndExperiment" should "call toDataBase and killSwitch with ShutdownMessage" in {
+    toDataBase.expects(*) once()
+    store ! StartOfExperiment
+    store ! EndOfExperiment(OptimizationResults(List.empty[Observation], Map.empty[String, Int], Observation(0,0)))
+    killSwitchProbe.expectMsg(KillSwitch.ShutdownMessage)
+  }
+}
