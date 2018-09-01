4e977c176c3bcc35e4e0f4975505fee6d0fc67eb
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-09-01 12:14:55 -0400

    Main now use ArgParser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,7 @@
-import config.ConfigFactory
-
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-
-    if(args.isEmpty) default
-    else {
-      args.head match  {
-        case "demoSampler" => DemoGreedySampler.makeDemo(1000)
-        case s: String => commandNotSupported
-      }
-    }
-
-    def default = {
-      val config = ConfigFactory.makeSquare
-      Experiment(config).run
-    }
-
-    def commandNotSupported = {
-      println("commandNotSupported")
-    }
+    val argParser = new ArgParser(args)
+    argParser.execute
   }
 }
 
